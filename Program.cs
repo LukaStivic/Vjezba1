e267848e675d52790bc08cee5a29c31209893826
LukaStivic <lstivi00@fesb.hr>
2026-03-24 19:09:14 +0100

    zadatak 16d

--- Program.cs
+++ Program.cs
@@ -1,14 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-
-namespace Vjezba1
-{
-    class Program
-    {
-        static void main(string[] args)
-        {
 Console.WriteLine("Hello, World!");
-            Console.WriteLine("Nešto novo");
-        }
-    }
-}
